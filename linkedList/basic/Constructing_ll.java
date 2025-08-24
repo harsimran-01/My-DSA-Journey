67134a2794b2d8b33b043e2ba3f5a92aa588db8c
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2025-08-24 13:12:35 +0530

    linked list operations

--- linkedList/basic/Constructing_ll.java
+++ linkedList/basic/Constructing_ll.java
@@ -13,14 +13,18 @@ public class Constructing_ll {
 
     public static Node head;
     public static Node tail;
+    public static int size;
+
 
     public void addFirst(int data){
         //step 1 - creating new Node
         Node newNode = new Node(data);
+        size++;
 
         //if list is empty
         if(head == null){
             head = tail = newNode;
+            return;
         }
 
         //step 2 - newnode.next = head
@@ -29,11 +33,117 @@ public class Constructing_ll {
         //step 3 - head = newNode;
         head = newNode;
     }
+    public void addLast(int data){
+        //step 1 - creating new Node
+        Node newNode = new Node(data);
+        size++;
+
+        //if list is empty
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        //step 2 - tail.next = newnode
+        tail.next = newNode;
+
+        //step 3 - tail = newnode
+        tail = newNode;
+    }
+
+    public void addMiddle(int indx,int data){
+        //creating newnode
+        if(indx==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i=0;
+        while(i<indx-1){
+            temp = temp.next;
+            i++;
+        }
+
+        //when the i = indx -1 means temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void printingLL(){
+        Node temp = head;
+
+        if(head == null){
+            System.out.println("List is Empty");
+            return;
+        }
+        while(temp != null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public int removeFirst(){
+        
+        if(size==0){
+            System.out.println("List is Empty nothing to remove");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size=0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast(){
+        
+        if(size==0){
+            System.out.println("List is Empty nothing to remove");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size=0;
+            return val;
+        }
+        Node temp = head;
+        for(int i=0;i<size-2;i++){
+            temp = temp.next;
+        }
+        int val = temp.next.data;
+        temp.next = null;
+        tail = temp;
+        size--;
+        return val;
+    }
 
     public static void main(String[] args) {
         Constructing_ll list = new Constructing_ll();
         list.addFirst(1);
+        list.printingLL();
         list.addFirst(2);
+        list.printingLL();
+        list.addLast(3);
+        list.printingLL();
+        list.addLast(4);
+        list.printingLL();
+        
+        list.addMiddle(2, 21);
+        list.printingLL();
+        System.out.println(list.size);
+        list.removeFirst();
+        list.printingLL();
+        System.out.println(list.size);
+        list.removeLast();
+        list.printingLL();
+        System.out.println(list.size);
     }
     
 }
