f013eb8aa0cc094c49eddfce30b5afe575b8a668
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2025-08-27 21:08:27 +0530

    stack

---
+++ stack/basic/creation.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+
+public class creation {
+    static class stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        public static void push(int data){
+            list.add(data);
+        } 
+
+        public static int pop(){
+            int val = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return val;
+        }
+
+        public static int peek(){
+            return list.get(list.size()-1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        stack s = new stack();
+        s.push(0);
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while(!s.isEmpty()){
+            System.out.print(s.peek()+ " ");
+            s.pop();
+        }
+    }
+}
