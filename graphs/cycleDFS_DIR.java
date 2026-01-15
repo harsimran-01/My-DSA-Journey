5a4a13d34cfb559a8687e3a95ad59f4e95d51ef7
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-01-15 20:09:48 +0530

    cycle detection

---
+++ graphs/cycleDFS_DIR.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class cycleDFS_DIR {
+    static class Edge {
+
+        int src;
+        int dest;
+
+        public Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 2));
+        graph[1].add(new Edge(1, 0));
+        graph[2].add(new Edge(2, 3));
+        graph[3].add(new Edge(3, 0));
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
