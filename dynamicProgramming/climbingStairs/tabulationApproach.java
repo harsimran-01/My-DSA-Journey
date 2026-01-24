206d72d087a013ec3a66816ed01ece241e46f641
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-01-24 17:24:18 +0530

    dp

---
+++ dynamicProgramming/climbingStairs/tabulationApproach.java
@@ -0,0 +1,22 @@
+
+public class tabulationApproach {
+
+    public static int countWays(int n, int dp[]) {
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                dp[i] = dp[i - 1] + 0;//i < 0 => 0
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int dp[] = new int[n + 1];
+        System.out.print(countWays(n, dp) + " ");
+    }
+}
--- dynamicProgramming/fibonacci.java
+++ dynamicProgramming/fibonacci.java
@@ -1,39 +1,58 @@
-package dynamicProgramming;
+// package dynamicProgramming;
 // package dynamic_programming;
 
 public class fibonacci {
+
+    //memoziation method
     //approach 1 -> time complexity -(exponential)
-    public static int fib(int n){
-        if(n==0||n==1){
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        return fib(n - 1) + fib(n - 2);
     }
-    
+
     //approach 2 -> time complexity -(linear) O(n)
-    public static int fib2(int n,int f[]){
-        if(n==0||n==1){
+    public static int fib2(int n, int f[]) {
+        if (n == 0 || n == 1) {
             return n;
         }
-        if(f[n] != 0){
+        if (f[n] != 0) {
             return f[n];
         }
-        f[n] = fib2(n-1, f) + fib2(n-2, f);
+        f[n] = fib2(n - 1, f) + fib2(n - 2, f);
         return f[n];
     }
-    
+
+    //tabulation method
+    public static int fibTab(int n) {
+        if (n == 0) return 0;
+        int dp[] = new int[n + 1];
+        // dp[0] = 0; //optional empty array is always 0
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
 
     public static void main(String[] args) {
         int n = 5;
         System.out.println("Appraoch 1 -exponential");
-        for(int i=0;i<=n;i++){
+        for (int i = 0; i <= n; i++) {
             System.out.print(fib(i) + " ");
         }
         System.out.println();
         System.out.println("Appraoch 2 -dynamic programming");
-        int f[] = new int[n+1]; //0 - n
-        for(int i=0;i<=n;i++){
-            System.out.print(fib2(i,f) + " ");
+        int f[] = new int[n + 1]; //0 - n
+        for (int i = 0; i <= n; i++) {
+            System.out.print(fib2(i, f) + " ");
+        }
+        System.out.println();
+        System.out.println("Appraoch 3 -dynamic programming using tabulation method");
+        for (int i = 0; i <= n; i++) {
+            System.out.print(fibTab(i) + " ");
         }
     }
 }
