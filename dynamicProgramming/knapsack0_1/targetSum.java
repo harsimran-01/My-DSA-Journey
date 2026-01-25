ac59ef8af4b8d6d951575849875c4375070b7d34
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-01-25 13:13:22 +0530

    target sum variation of 0 1 knapsack

--- dynamicProgramming/knapsack0_1/targetSum.java
+++ dynamicProgramming/knapsack0_1/targetSum.java
@@ -1,5 +1,5 @@
 public class targetSum {
-    public static void print(int dp[][]){
+    public static void print(boolean dp[][]){
         for(int i=0;i<dp.length;i++){
             for(int j=0;j<dp[0].length;j++){
                 System.out.print(dp[i][j] +  " ");
@@ -7,50 +7,45 @@ public class targetSum {
             System.out.println();
         }
     }
-    public static int knapsack(int val[], int wt[], int indx, int W, int dp[][]) {
+    public static boolean targetSumCode(int val[],int sum,boolean  dp[][]) {
         int n = val.length;
-        for (int i = 0; i < dp.length; i++) { //0th col
-            dp[i][0] = 0;
-        }
-        for (int j = 0; j < dp[0].length; j++) {//0th row
-            dp[0][j] = 0;
+
+        for(int i=0;i<n+1;i++){
+            dp[i][0] = true;
         }
 
-        //initialization
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < W + 1; j++) {
-                int v = val[i - 1];
-                int w = wt[i - 1];
-                if (w <= j) { //valid
-                    //case 1
-                    int profit1 = v + dp[i - 1][j - w];
-                    //case 2
-                    int profit2 = dp[i - 1][j];
-                    dp[i][j] = Math.max(profit1, profit2);
-                    
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                
+        // for(int j=0;j<sum+1;j++){
+        //     dp[0][j] = false;
+        // }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                //include
+                int v = val[i-1];
+                if(v<=j && dp[i-1][j-v] == true){
+                    dp[i][j] = true;
+                }
+                //exclude
+                else if(dp[i-1][j] == true){
+                    dp[i][j] = true;
                 }
             }
         }
-        //printing
         print(dp);
-        return dp[n][W];
+        return dp[n][sum];
 
     }
 
     public static void main(String[] args) {
-        int val[] = {15, 14, 10, 45, 30};
-        int wt[] = {2, 5, 1, 3, 4};
-        int W = 7;
-        int dp[][] = new int[val.length + 1][W + 1];
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[0].length; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        System.out.print(knapsack(val, wt, val.length, W, dp));
+        int val[] = {4,2,7,1,3};
+        int sum = 10;
+        boolean dp[][] = new boolean[val.length + 1][sum + 1];
+        // for (int i = 0; i < dp.length; i++) {
+        //     for (int j = 0; j < dp[0].length; j++) {
+        //         dp[i][j] = false;
+        //     }
+        // }
+        System.out.print(targetSumCode(val,sum,dp));
 
     }
 }
