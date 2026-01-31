52f45e2327f859890683fb2c7f83fab40e97aab0
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-01-31 12:19:57 +0530

    catalan;s number recursion approach

---
+++ dynamicProgramming/catalansNumber/recursion.java
@@ -0,0 +1,18 @@
+// package dynamicProgramming.catalansNumber;
+
+public class recursion {
+    public static int catalan_Number(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        int ans = 0;
+        for(int i=0;i<=n-1;i++){
+            ans += catalan_Number(i) * catalan_Number(n-i-1);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(catalan_Number(4));
+    }
+}
