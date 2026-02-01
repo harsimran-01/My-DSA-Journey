63e7cfb3e5a4ee3b24929cfd74309d6f6dbced77
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2026-02-01 13:08:16 +0530

    max/min element in range

--- segmentTree/max_min.java
+++ segmentTree/max_min.java
@@ -15,6 +15,24 @@ public class max_min {
         tree[i] = Math.max(tree[2*i+1], tree[2*i+2]);
         
     }
+    public static int getMaxUtil(int i,int si,int sj,int qi,int qj){
+        //no overlap
+        if(si>qj || sj<qi){
+            return Integer.MIN_VALUE;
+        }else if(si>=qi && sj<=qj){ //complete overlap
+            return tree[i];
+        }else{//partial overlap
+            int mid = (si+sj)/2;
+            int left = getMaxUtil(2*i+1, si, mid, qi, qj);
+            int right = getMaxUtil(2*i+2, mid+1,sj, qi, qj);
+            return Math.max(left, right);
+        }
+
+    }
+    public static int getMax(int arr[],int qi,int qj){
+        int n = arr.length;
+        return getMaxUtil(0, 0, n-1, qi, qj);
+    }
     public static void main(String[] args) {
         int arr[] = {6,8,-1,2,17,1,3,2,4};
         init(arr.length);
@@ -23,5 +41,8 @@ public class max_min {
             System.out.print(tree[i] + " ");
         }
         System.out.println();
+
+        int max = getMax(arr,2,5);
+        System.out.println(max);
     }
 }
